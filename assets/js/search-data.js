cae3562138596c5b28d5c47c02dc5b7d7d5dcec9
iitanmay <iitanmay@users.noreply.github.com>
2025-11-25 10:25:13 +0000

    deploy: 76f86cc87bfa002953174617bd48df3c4c22cc89

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "",
+          description: "My efforts towards dissemination of the accumulated knowledge",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
